94ef1ca798737ff3cdb678f61436f91513e70a9c
Neema-Charity <ncharityyy@gmail.com>
2024-05-27 22:14:53 +0300

    code challenge

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,95 @@
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
-        self.title = title
-        
+        self._title = title  
+        Article.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, new_title):
+        raise AttributeError("Title cannot be changed")
+
+
 class Author:
     def __init__(self, name):
-        self.name = name
+        self._name = name  
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        raise AttributeError("Name cannot be changed")
 
     def articles(self):
-        pass
+        return [article for article in Article.all if self == article.author]
 
     def magazines(self):
-        pass
+        return list({article.magazine for article in self.articles()})
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        if not self.articles():
+            return None
+        return list({magazine.category for magazine in self.magazines()})
+
 
 class Magazine:
     def __init__(self, name, category):
         self.name = name
         self.category = category
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        if not isinstance(new_name, str):
+            raise TypeError("Name must be a string")
+        if not 2 <= len(new_name) <= 16:
+            raise ValueError("Name must be between 2 and 16 characters")
+        self._name = new_name
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, new_category):
+        if not isinstance(new_category, str):
+            raise TypeError("Category must be a string")
+        if len(new_category) == 0:
+            raise ValueError("Category must be longer than 0 characters")
+        self._category = new_category
+
     def articles(self):
-        pass
+        return [article for article in Article.all if self == article.magazine]
 
     def contributors(self):
-        pass
+        return list({article.author for article in self.articles()})
 
     def article_titles(self):
-        pass
+        titles = [article.title for article in self.articles()]
+        return titles if titles else None
 
     def contributing_authors(self):
-        pass
+        author_count = {}
+        for article in self.articles():
+            author = article.author
+            if author in author_count:
+                author_count[author] += 1
+            else:
+                author_count[author] = 1
+
+        authors = [author for author, count in author_count.items() if count >= 2]
+        return authors if authors else None
